21f2a3dc9f8c00be9ec8dd76e22abb384cef0081
Mariel Pacada <ann_mariel_pacada@brown.edu>
2020-10-16 23:49:13 -0400

    okay now

--- script.js
+++ script.js
@@ -50,10 +50,19 @@ function search() {
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("sidenav-button").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
+    document.getElementById("sidenav-button").style.cursor = "default";
+    document.getElementById("sidenav-button").style.filter = "grayscale(100%)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("sidenav-button").style.marginLeft = "0";
-  }
+    document.getElementById("sidenav-button").style.cursor = "pointer";
+    document.getElementById("sidenav-button").style.filter = "grayscale(0%)";
+
+}
+
+
+
+
