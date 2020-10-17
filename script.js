adc77c940ea56a4761ff8167ec9cdaf2db1c1de8
Mariel Pacada <ann_mariel_pacada@brown.edu>
2020-10-16 23:31:02 -0400

    i think im done done

--- script.js
+++ script.js
@@ -49,11 +49,11 @@ function search() {
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidenav-button").style.marginLeft = "250px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("sidenav-button").style.marginLeft = "0";
   }
